ff0b27d40954496c946ece588d8c8511b8aa6d79
masataka kanno <akapanna@gmail.com>
2013-05-13 18:05:05 +0900

    テストデータ範囲を増やした

--- teihon-java/chapter17/counting_sort.py
+++ teihon-java/chapter17/counting_sort.py
@@ -4,8 +4,8 @@ import unittest
 # 書籍とは違う実装になった。
 # 分布図を使っていないんだけど、ロジック勘違いしている？
 def sort(buf):
-    # ビンのサイズは10で固定とする
-    bin_size = 10
+    # ビンのサイズは100で固定とする
+    bin_size = 100
     bin = [0] * bin_size
     for i in range(len(buf)):
         x = buf[i]
@@ -24,6 +24,7 @@ class Test(unittest.TestCase):
         self.assertEquals(sort([1, 2]), [1, 2])
         self.assertEquals(sort([2, 1]), [1, 2])
         self.assertEquals(sort([3, 3, 6, 1, 3, 2, 5, 7, 5, 4]), [1, 2, 3, 3, 3, 4, 5, 5, 6, 7])
+        self.assertEquals(sort([30, 30, 61, 12, 3, 21, 53, 7, 53, 4]), [3, 4, 7, 12, 21, 30, 30, 53, 53, 61])
 
 if __name__ == '__main__':
     unittest.main()
