e221dff7bb0a73fa0a0799da0c72397d47deb2b8
masataka kanno <akapanna@gmail.com>
2013-05-13 17:20:07 +0900

    分布数え上げソート実装

---
+++ teihon-java/chapter17/counting_sort.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+def sort(buf):
+    # ビンのサイズは10で固定とする
+    bin_size = 10
+    bin = [0] * bin_size
+    for i in range(len(buf)):
+        x = buf[i]
+        bin[x] += 1
+
+    j = 0
+    for i in range(bin_size):
+        for n in range(bin[i]):
+            buf[j] = i
+            j += 1
+    return buf
+
+class Test(unittest.TestCase):
+    def test_sort(self):
+        self.assertEquals(sort([1]), [1])
+        self.assertEquals(sort([1, 2]), [1, 2])
+        self.assertEquals(sort([2, 1]), [1, 2])
+        self.assertEquals(sort([3, 3, 3, 6, 1, 2, 5, 5, 7, 4]), [1, 2, 3, 3, 3, 4, 5, 5, 6, 7])
+
+if __name__ == '__main__':
+    unittest.main()
+
