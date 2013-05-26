d60c623b8e8d2e32fe4ec4c8ad1b8c7d066c74d6
masataka kanno <akapanna@gmail.com>
2013-05-26 10:12:15 +0900

    配列パターン

--- saikyou-saisoku/chapter05/interesting_party.py
+++ saikyou-saisoku/chapter05/interesting_party.py
@@ -2,15 +2,14 @@
 import unittest
 
 def solve(first, second):
-    return s1(first, second)
-
-def s1(first, second):
     num = len(first)
     if num < 1:
         return 0
     elif num == 1 and first[0] == '':
         return 0
+    return s2(num, first, second)
 
+def s1(num, first, second):
     talks = {}
     for i in xrange(num):
         talks[first[i]] = 0
@@ -21,6 +20,20 @@ def s1(first, second):
                 v += 1
         talks[k] = v
     return max(talks.values())
+
+def s2(num, first, second):
+    ans = 0
+    for i in xrange(num):
+        f = 0
+        s = 0
+        for j in xrange(num):
+            if first[i] == first[j]:   f += 1
+            if first[i] == second[j]:  f += 1
+            if second[i] == first[j]:  s += 1
+            if second[i] == second[j]: s += 1
+        ans = max([f, ans])
+        ans = max([s, ans])
+    return ans
 
 class Test(unittest.TestCase):
     def test(self):
