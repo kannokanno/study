07abb8f86cc72e4e55bfc3dbd615733009a52f76
masataka kanno <akapanna@gmail.com>
2013-06-18 13:52:16 +0900

    prototype

--- design-patterns-in-python/singleton.py
+++ design-patterns-in-python/singleton.py
@@ -1,18 +1,32 @@
-class Hoge:
-    __instance__ = None
+from copy import deepcopy
 
-    def get_instance():
-        if Hoge.__instance__ is None:
-            Hoge.__instance__ = Hoge()
-        return Hoge.__instance__
 
-    get_instance = staticmethod(get_instance)
+class Manager:
+    def __init__(self):
+        self._prototypes = {}
 
+    def register(self, key, prototype):
+        self._prototypes[key] = prototype
+
+    def create(self, key):
+
+        return deepcopy(self._prototypes[key])
+
+
+class Marker:
+    def __init__(self, mark):
+        self.mark = mark
+
+    def use(self, str):
+        return self.mark + str + self.mark
 
 if __name__ == '__main__':
-    a = Hoge.get_instance()
-    b = Hoge.get_instance()
-    print a
-    print b
-    print a == b
-    print a is b
+    manager = Manager()
+    manager.register('marker', Marker('*'))
+
+    strong = manager.create('marker')
+    print strong.use('Hello')
+
+    delete = manager.create('marker')
+    delete.mark = '-'
+    print delete.use('Hello')
