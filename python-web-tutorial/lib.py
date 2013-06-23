ecc21fcdf15572c25e8c411377efb2a0626aab36
masataka kanno <akapanna@gmail.com>
2013-06-23 10:35:24 +0900

    yeah

--- python-web-tutorial/lib.py
+++ python-web-tutorial/lib.py
@@ -4,14 +4,20 @@
 #print "\n".join(dir(os))
 
 
-# doctest
-def average(values):
-    """Computes the arithmetic mean of a list of numbers.
+## doctest
+#def average(values):
+#    """Computes the arithmetic mean of a list of numbers.
+#
+#    >>> print average([20, 30, 70])
+#    40.0
+#    """
+#    return sum(values, 0.0) / len(values)
+#
+#import doctest
+#doctest.testmod()   # 組み込まれたテストを自動的に検証する。
 
-    >>> print average([20, 30, 70])
-    40.0
-    """
-    return sum(values, 0.0) / len(values)
-
-import doctest
-doctest.testmod()   # 組み込まれたテストを自動的に検証する。
+## logging
+import logging
+logging.debug('hogehoge')
+logging.warning('hogehoge')
+logging.error('hogehoge')
