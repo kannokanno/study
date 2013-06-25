5cc391cea4dea2eaf232673f4de608933c4cf075
masataka kanno <akapanna@gmail.com>
2013-06-25 18:45:44 +0900

    visitorパターンの認識を間違えていたので修正

--- design-patterns-in-python/visitor.py
+++ design-patterns-in-python/visitor.py
@@ -1,46 +1,67 @@
 class Entry:
-    def __init__(self, name, size):
-        self.entries = []
+    def __init__(self, name):
         self.name = name
-        self.size = size
 
     def add(self, entry):
         # do nothing
         pass
 
-    def accept(self, visitor):
-        return visitor.visit(self)
-
 class Directory(Entry):
+    def __init__(self, name):
+        Entry.__init__(self, name)
+        self.entries = []
+
     def add(self, entry):
         self.entries.append(entry)
 
-class File(Entry):
-    pass
+    def accept(self, visitor):
+        return visitor.visit_dir(self)
 
-class PrintNameVisitor:
+class File(Entry):
+    def __init__(self, name, size):
+        Entry.__init__(self, name)
+        self.size = size
+
+    def accept(self, visitor):
+        return visitor.visit_file(self)
+
+class BadNamePrinter:
     def visit(self, element):
+        print element.name
+        if hasattr(element, 'entries'):
+            for e in element.entries:
+                self.visit(e)
+
+class NamePrinter:
+    def visit_file(self, element):
+        print element.name
+
+    def visit_dir(self, element):
         print element.name
         for e in element.entries:
             e.accept(self)
 
-class CalcTotalSizeVisitor:
-    def visit(self, element):
-        total = element.size
+class SizeCalculator:
+    def visit_file(self, element):
+        return element.size
+
+    def visit_dir(self, element):
+        total = 0
         for e in element.entries:
             total += e.accept(self)
         return total
 
 if __name__ == '__main__':
-    d1 = Directory('Directory A', 1000)
+    d1 = Directory('Directory A')
     d1.add(File('File A', 200))
     d1.add(File('File B', 300))
     d1.add(File('File C', 500))
 
-    d2 = Directory('Directory B', 2000)
+    d2 = Directory('Directory B')
     d2.add(File('File A', 2000))
-    d2.add(d1)
+    d1.add(d2)
 
-    d2.accept(PrintNameVisitor())
+    d1.accept(NamePrinter())
+    BadNamePrinter().visit(d1)
     print "--------------"
-    print "SIZE:" + str(d2.accept(CalcTotalSizeVisitor()))
+    print "SIZE:" + str(d1.accept(SizeCalculator()))
